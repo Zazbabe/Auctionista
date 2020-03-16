16a8bb5782923581b78b09ef24221a3ad124e6be
Lamafoos <Per>
2020-03-16 13:45:26 +0100

    Revert "Users commit"
    This reverts commit bcc05731334ad6bf210ebb8899af50a99074bcf7.

--- src/main/java/com/example/demo/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends CrudRepository<User, Integer> {
-}
