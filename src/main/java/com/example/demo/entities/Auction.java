b4d1302cf079b6813032dcdd43e46eb5546d5871
Johan Mattsson <johan.mattsson@hotmail.se>
2020-03-19 10:06:58 +0100

    Create entities/Auction.java

--- src/main/java/com/example/demo/entities/Auction.java
+++ src/main/java/com/example/demo/entities/Auction.java
@@ -1,4 +1,87 @@
 package com.example.demo.entities;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "auctions")
 public class Auction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int seller;
+    private String title;
+    private String description;
+    private double reserve_price;
+    private Date start_time;
+    private Date end_time;
+    private int main_image;
+
+    public Auction() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSeller() {
+        return seller;
+    }
+
+    public void setSeller(int seller) {
+        this.seller = seller;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getReserve_price() {
+        return reserve_price;
+    }
+
+    public void setReserve_price(double reserve_price) {
+        this.reserve_price = reserve_price;
+    }
+
+    public Date getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Date start_time) {
+        this.start_time = start_time;
+    }
+
+    public Date getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Date end_time) {
+        this.end_time = end_time;
+    }
+
+    public int getMain_image() {
+        return main_image;
+    }
+
+    public void setMain_image(int main_image) {
+        this.main_image = main_image;
+    }
 }
