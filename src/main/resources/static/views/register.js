c21c161a82e943b1541173903d03669ad5073d51
Lamafoos <Per>
2020-03-23 17:10:16 +0100

    small edit

--- src/main/resources/static/views/register.js
+++ src/main/resources/static/views/register.js
@@ -1,16 +1,14 @@
 import newUser from '../components/addNewUser.js'
-import login from '../components/login.js'
+
 
 export default {
     components: {
-        newUser,
-        login
+        newUser
     },
     template: `
         <div>
             <h2> Register User </h2>
             <newUser> </newUser>
-            <login> </login>
         </div>
     `
 }
