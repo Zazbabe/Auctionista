01092268f745118e3ef6db298e47584776bb63c4
Lamafoos <Per>
2020-03-23 11:05:22 +0100

    register user

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,15 +1,30 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class UserController {
 
-/*
- @PostMapping("/rest/users")
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/rest/users")
+    public List<User> gettAllUsers() {
+        return userService.findAllUser();
+    }
+    @GetMapping("/rest/users/{id}")
+    public Optional<User> getUserById(@PathVariable int id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping("/rest/users")
     public User createNewUser(@RequestBody User user) {
      return userService.createNewUser(user);
- } /
- */
+    }
 }
--- src/main/resources/static/views/about.js
+++ src/main/resources/static/views/about.js
@@ -1,7 +1,13 @@
+import newUser from '../components/addNewUser.js'
+
 export default {
+    components: {
+        newUser
+    },
     template: `
         <div>
             <h2> Abooot </h2>
+            <newUser> </newUser>
         </div>
     `
 }
