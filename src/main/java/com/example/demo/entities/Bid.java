a367be871e34eb37cbc79571c7acff09c572cdc5
Faris Ali <faris.assir@gmail.com>
2020-03-25 09:03:17 +0100

    Create  Bid Class attributes / constructor / getter&setter

--- src/main/java/com/example/demo/entities/Bid.java
+++ src/main/java/com/example/demo/entities/Bid.java
@@ -1,4 +1,61 @@
 package com.example.demo.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "bids")
 public class Bid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id ;
+    private double bid;
+    private int bidder ;
+    private String bid_time;
+    private int auction_id;
+
+    public Bid(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id=id;
+    }
+
+    public double getBid() {
+        return bid;
+    }
+
+    public void setBid(double bid) {
+        this.bid=bid;
+    }
+
+    public int getBidder() {
+        return bidder;
+    }
+
+    public void setBidder(int bidder) {
+        this.bidder=bidder;
+    }
+
+    public String getBid_time() {
+        return bid_time;
+    }
+
+    public void setBid_time(String bid_time) {
+        this.bid_time=bid_time;
+    }
+
+    public int getAuction_id() {
+        return auction_id;
+    }
+
+    public void setAuction_id(int auction_id) {
+        this.auction_id=auction_id;
+    }
 }
