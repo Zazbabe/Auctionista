fd36547a4f7bdccf91067701bb385df0cacfc4e3
Faris Ali <faris.assir@gmail.com>
2020-03-25 21:21:39 +0100

    Create methods to fetch the current bid data

--- src/main/java/com/example/demo/services/BidService.java
+++ src/main/java/com/example/demo/services/BidService.java
@@ -1,4 +1,29 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.Bid;
+import com.example.demo.repositories.BidRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BidService {
-}
+        @Autowired
+        BidRepo bidRepo;
+
+//        @Autowired
+//    AuctionRepo auctionRepo;
+
+        public List<Bid> findAllCurrentBids(){
+            return (List<Bid>) bidRepo.findAll();
+        }
+
+        public Bid findHighestBidByAuctionId(int id) {
+            return bidRepo.findById(id);
+        }
+
+
+
+    }
+
