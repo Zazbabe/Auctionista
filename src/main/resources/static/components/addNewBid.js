86189df4a4e73ab517e91357d20f293fc41b6f2d
Faris Ali <faris.assir@gmail.com>
2020-04-01 19:15:35 +0200

    working on component

--- src/main/resources/static/components/addNewBid.js
+++ src/main/resources/static/components/addNewBid.js
@@ -2,12 +2,52 @@ export default {
     template: `
   <form @submit.prevent = "addNewBid">
   <label>Bid here: </label>
-  <input type="bid" placeholder="add higher bid.."input>
+  <input v-model = "bid" type="double" placeholder="add higher bid.."input>
   <button>Add bid</button>
   </form>
-  <p>You bid: {{bid.bid}}</p>
+  <!-- <p>You bid: {{bid.bid}}</p> -->
   `,
 
+data() {
+    return {
+        // lägg till att hitta inloggad
+        bid: '',
+        bidder: '',
+        bid_time: '',
+        auction_id: '',
+    }
+    },
+
+    methods: {
+        async addNewBid() {
+
+            // LÄGG TILL FÖR KORT LÖSEN MM
+            let NewBid = {
+                bid: this.bid,
+                bidder: this.$store.state.user.id,
+                bid_time: this.bid_time,
+                // auction_id: this.$store.state.auctions.id
+
+            }
+            let result = await fetch('/rest/bids', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(NewBid)
+            })
+            result = await result.json()
+            this.$store.commit('appendBid', result)
+
+        }
+            
+              
+
+
+
+
+
+}
 
 
 }
